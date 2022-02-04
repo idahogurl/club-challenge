fbbf3d56f51eec6e22992eccd4fab0b1c67550bb
Rebecca Vest <rvest@equinoxmedia.com>
2022-02-03 21:45:44 -0700

    Make header fit mobile screen

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,11 +1,11 @@
-import logo from '../../logo.png';
+import logo from '../../Abbv_Equinox-Black_L 1.png';
 import styles from './header.module.css';
 
 export const Header = () => {
   return (
     <header className={styles.header}>
       <img src={logo} className={styles.headerLogo} alt="logo" />
-      <p className={styles.headerText}>Employee Experience</p>
+      <span className={styles.headerText}>Employee Exp</span>
     </header>
   );
 }
