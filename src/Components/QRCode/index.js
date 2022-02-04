de8a95c7c8f61f73b0a21f1d9ae776bd355b91a8
Rebecca Vest <rvest@equinoxmedia.com>
2022-02-03 22:13:35 -0700

    Use query param for referral QR code

--- src/Components/QRCode/index.js
+++ src/Components/QRCode/index.js
@@ -5,25 +5,28 @@ import QRCodeComponent  from "react-qr-code";
 import styles from './qrcode.module.css';
 import logoImage from '../../Equinox+_White_sm 1.png';
 
-function QRSlide({ title, text, value }) {
+function QRCodeSlide({ title, text, codeValue }) {
   return <div className={styles.qrCodeSlide}>
       <h3 className={styles.headerText}>{title}</h3>
       <p>{text}</p>
       <div className={styles.qrCodeWrapper}>
           <img src={logoImage} className={styles.qrCodeLogo} alt=""/>
           <div className={styles.qrCode}>
-            <QRCodeComponent value={value} size={151} />
+            <QRCodeComponent value={codeValue} size={151} />
           </div>
       </div>
     </div>;
 }
 
-export const QRCode = ({ value }) => {
+export const QRCode = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const referrerId = urlParams.get("referrerId");
+
   return (
     <div className={styles.container}>
       <Carousel>
-        <QRSlide value={value} title="Refer Friends &amp; Family" text="Scan QR to load the referral form." />
-        <QRSlide value={value} title="Sign Up Online" text="Scan QR to load the sign up form to join online." />
+        <QRCodeSlide codeValue={`https://www.equinox.com/referrals/${referrerId}`} title="Refer Friends &amp; Family" text="Scan QR to load the referral form." />
+        <QRCodeSlide codeValue="https://www.equinox.com/join/step1/115?icmp=topnav-join" title="Sign Up Online" text="Scan QR to load the sign up form to join online." />
       </Carousel>
     </div>
   );
